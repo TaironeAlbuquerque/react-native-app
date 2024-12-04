29bc7a5ca5a471f0e538e7e606212f2f2acd5590
Tairone Albuquerque <57819069+TaironeAlbuquerque@users.noreply.github.com>
2024-12-03 21:48:29 -0300

    Add files via upload

---
+++ assets/data.js
@@ -0,0 +1,12 @@
+export const dummyData =
+        [{
+                title: 'Texto 1', url: 'https://i.ibb.co/hYjK44F/anise-aroma-art-bazaar-277253.jpg',
+                description: "Isso é uma breve descrição de exemplo. Aqui você pode customizar",
+                id: 1
+
+        },
+        {
+                title: 'Texto 2', url: 'https://i.ibb.co/JtS24qP/food-inside-bowl-1854037.jpg',
+                description: "Isso é uma breve descrição de exemplo. Aqui você pode customizar",
+                id: 2
+        }]
